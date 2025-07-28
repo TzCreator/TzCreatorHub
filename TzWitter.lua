967c1b10ab1dfb9efd5a0be33faaa4fc3e157b31
TeryousDev777 <x7klzinx@gmail.com>
2025-07-28 15:59:40 -0300

    Update TzWitter.lua

--- TzWitter.lua
+++ TzWitter.lua
@@ -10,7 +10,7 @@ local Window = Libary:MakeWindow({
     Flags = "Tzub_Broookhaven"
 })
 Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://131669852271916", BackgroundTransparency = 0 },
+    Button = { Image = "rbxassetid://8964489619", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
